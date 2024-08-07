07185b1ff51987333eee75e3164e510c8a19c736
vladholomah <vlad_holomah@ukr.net>
2024-08-07 09:11:36 +0300

    Update ExchangeDisplay and styles25

--- src/index.tsx
+++ src/index.tsx
@@ -4,16 +4,25 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+declare global {
+  interface Window {
+    Telegram: any;
+  }
+}
+
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
+
+// Налаштування Telegram Web App
+const tg = window.Telegram.WebApp;
+tg.expand();
+tg.setHeaderColor('secondary_bg_color');
+
 root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
